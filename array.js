a24ba86f0320d7ea24d1d038a442996ddd75e30a
Aman Pandey <pandeyaman636@gmail.com>
2024-09-06 01:08:54 +0530

    practice for basic question of array

--- array.js
+++ array.js
@@ -1,2 +1,14 @@
 let fruits = ['apple', 'ornage', 'pine apple', 'mango']
 console.log(fruits)
+console.log(fruits[1])
+fruits[1]='banana'
+console.log(fruits);
+fruits.push('cherry')
+console.log(fruits)
+fruits.splice(fruits.indexOf('pine apple'),1)
+console.log(fruits)
+fruits.unshift('mango')
+console.log(fruits)
+fruits.splice(0, 4, "lemon")
+console.log(fruits)
+console.log(fruits.length);
